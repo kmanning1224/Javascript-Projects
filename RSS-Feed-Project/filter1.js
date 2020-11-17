7dd3812ed388eca96b4183f93139e4a28b0f8fd9
Katherine Manning <64861134+kmanning1224@users.noreply.github.com>
2020-11-17 13:45:46 -0500

    RSS-Project import

---
+++ RSS-Feed-Project/filter1.js
@@ -0,0 +1,21 @@
+var url = 'https://siftrss.com/f/nPxGLmKzA1'
+const textarea = document.querySelector('#feed-textarea > ul');
+  feednami.loadGoogleFormat(url,function(result){
+    if(result.error){
+      console.log(result.error)
+    }
+    else{
+        
+        var entries = result.feed.entries
+        for(var i = 0; i < entries.length; i++){
+          textarea.value = ''
+          var entry = entries[i]
+          let li = document.createElement('li');
+          li.innerHTML = `<h4> <a href="${entry.link}">${entry.title}</a></h4> <h5><i>" ${entry.contentSnippet}...."</h5>`
+          textarea.appendChild(li);
+        console.log(entry.title)
+        console.log(entry.contentSnippet) 
+        // the first 120 characters of the entry
+      }
+    }
+  })
---
+++ RSS-Feed-Project/filter3.js
@@ -0,0 +1,19 @@
+var url = 'https://siftrss.com/f/dgQVMee9oAo'
+const textarea = document.querySelector('#feed-textarea > ul');
+  feednami.loadGoogleFormat(url,function(result){
+    if(result.error){
+      console.log(result.error)
+    }
+    else{
+        
+        var entries = result.feed.entries
+        for(var i = 0; i < entries.length; i++){
+          textarea.value = ''
+          var entry = entries[i]
+          let li = document.createElement('li');
+          li.innerHTML = `<h4> <a href="${entry.link}">${entry.title}</a></h4> <h5><i>" ${entry.contentSnippet}...."</h5>`
+          textarea.appendChild(li); 
+        // the first 120 characters of the entry
+      }
+    }
+  })
